4d33c4766f8db77dbda089d24e3e5a1dec6bdc73
AIabdoPr <abdopr47@gmail.com>
2023-07-25 19:41:27 +0100

    Our Printf Function

--- convert_and_print.c
+++ convert_and_print.c
@@ -6,7 +6,7 @@
  * @n: Number to be converted and printed (unsigned long int)
  * @base: Base for conversion (int)
  * @bsize: Minimum number of digits to print (int)
- * @upper_on_hex: Flag to print uppercase characters in hexadecimal (int)
+ * @upper_hex: Flag to print uppercase characters in hexadecimal (int)
  * Return: Length of printed text (int)
  */
 int convert_and_print(unsigned long int n, int base, int bsize, int upper_hex)
@@ -32,7 +32,7 @@ int convert_and_print(unsigned long int n, int base, int bsize, int upper_hex)
 	{
 		d = n % base;
 		if (base == 16 && d > 9)
-			sn[i] = (upper_on_hex == 1 ? 'A' : 'a') + d - 10;
+			sn[i] = (upper_hex == 1 ? 'A' : 'a') + d - 10;
 		else
 			sn[i] = '0' + d;
 		n = n / base;
